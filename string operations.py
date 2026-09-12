a48d786c18286d6ae2cc841b57c8d298ce038533
Supraja <deshaipetasupraja@gmail.com>
2026-09-12 22:58:07 +0530

    Practicing string operations

---
+++ string operations.py
@@ -0,0 +1,30 @@
+str="Hello, World!"
+print(str)
+print(len(str))
+
+s="python programming"
+print(s.upper())
+
+a="Data Science"
+print(a[0:5])
+
+c="I am learning Python"
+print(c.find("Python"))
+
+u="OpenAI"
+print(u[::5])
+
+d="I love Java programming"
+print(d.replace("Java","python"))
+
+lst=["Hello "," World"]
+print(''.join(lst))
+
+e="Hello, how are you?"
+print(e.count("o"))
+
+f="apple, banana, grape"
+print(f.split())
+
+g=" python"
+print(g.strip())
