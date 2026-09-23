f11900b0826f40e6a9ae9909de80c1618ed3646a
Supraja <deshaipetasupraja@gmail.com>
2026-09-23 22:35:31 +0530

    Learing Python Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,20 @@
+dict={"name":"Supraja","age":21,"city":"Hyderabad"}
+print(dict)
+print(dict["name"])
+print(dict["age"])
+print(dict["city"])
+dict["age"]=22
+print(dict)
+
+info={
+    "subjects":["Python","Java"],
+    "topics":("dict","set","list")
+}
+print(info)
+print(type(info))
+print(info["subjects"])
+print(info["topics"])
+
+null_dict={}
+null_dict["name"]="Sujana"
+print(null_dict)
