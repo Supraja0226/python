d4559a4e52c137c442f90dc3f298560ceb22820c
Supraja <deshaipetasupraja@gmail.com>
2026-09-24 11:47:22 +0530

    Learning Python Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -21,3 +21,33 @@ null_dict={}
 null_dict["name"]="Sujana"
 print(null_dict)
 
+#nested dictionary
+student={
+    "name":"Supraja",
+    "subjects":{
+        "phy":94,
+        "chem":96,
+        "maths":95
+    }
+}
+print(student)
+print(student["subjects"])
+print(student["subjects"]["chem"])
+
+#Dictionary methods
+print(student.keys())#returns all keys
+print(student.values())#returns all values
+print(student.items())#returns all (key, val)pairs as tuples
+print(student.get("name"))#returns the key according to value
+student.update({"age":17})#inserts the specified items to the dictionary
+print(student)
+print(student.pop("age"))#removes the speified key and returns its value 
+print(student.popitem())#removes and returns an arbitary pair from the dictionary
+new_dict=student.copy()#returns a shallow copy of the dictionary
+print(new_dict)
+print(len(student))#returns the number of items in ith dictionary
+
+
+
+student.clear()#removes all items from the dictionary
+print(student)
